e1cde0c2f46f0f61cb1ff2412d00fa99072583a8
Matt <mattschlosser@users.noreply.github.com>
2025-05-11 15:46:20 -0600

    feat: clicking on any bus shows all stops along route

--- src/api/index.js
+++ src/api/index.js
@@ -37,7 +37,7 @@
 
 /**
  * Gets a list of 1000 bus stops from the edmonton data api
- * @param {nunber} offset 
+ * @param {number} offset 
  * 
  */
 const getBusStops = async (offset = null) => {
@@ -73,10 +73,47 @@ export const getAllBusStops = async () => {
     return stops;
 }
 
+/** 
+ * @typedef {Object} BusStopTime 
+ * @property {string} trip_id Identifies a trip.
+ * @property {string} arrival_time 	
+Arrival time at a specific stop for a specific trip on a route. For times occurring after midnight on the service day, the time has a value greater than 24:00:00 in HH:MM:SS local time for the day on which the trip schedule begins.
+ * @property {string} departure_time 	
+Departure time from a specific stop for  a specific trip on a route. For times occurring after midnight on the service day, the time has a value greater than 24:00:00 in HH:MM:SS local time for the day on which the trip schedule begins.
+ * @property {string} stop_id Identifies the serviced stop.
+ * @property {string} stop_sequence 	
+Order of stops for a particular trip. The values must increase along the trip but do not need to be consecutive.
+ * @property {string} drop_off_type 	
+Indicates drop off method. Valid options are: 0 or empty - Regularly scheduled drop off. 1 - No drop off available. 2 - Must phone agency to arrange drop off. 3 - Must coordinate with driver to arrange drop off.
+ * @property {string} arrival_time_fixed Not from the GTFS feed. For times occurring after midnight on the service day, times have values greater than 24:00:00; this column represents such times normally (i.e. for hours >= 24, 24 is subtracted)
+ * @property {string} departure_time_fixed Not from the GTFS feed. For times occurring after midnight on the service day, times have values greater than 24:00:00; this column represents such times normally (i.e. for hours >= 24, 24 is subtracted)
+ * 
+ */
+
+/**
+ * 
+ * @param {string} tripId The trip id to get bus stop times for
+ * @returns {Promise<BusStopTime[]>} 
+ */
+export const getBusStopTimes = async (tripId) => {
+    let url = `https://data.edmonton.ca/resource/greh-g7ac.json?trip_id=${tripId}`;
+        
+    let result = await fetch(url, {
+        headers: {
+            "Accept": "application/json",
+            "X-App-Token": import.meta.env.VITE_APP_SODA_APP_TOKEN
+        }
+    })
+    if (result.ok) {
+        return result.json();
+    }
+    return [];
+}
+
 /**
  * 
  * @param {} tripId 
- * @returns {Trip}
+ * @returns {Promise<Trip | null>}
  */
 export const getTrip = async (tripId) => {
     let result = await  fetch(`https://data.edmonton.ca/resource/ctwr-tvrd.json?trip_id=${tripId}`, {
@@ -90,6 +127,6 @@ export const getTrip = async (tripId) => {
         if (trip) {
             return trip;
         }
-        return null;
     }
+    return null;
 }
